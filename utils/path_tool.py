d9a9872a393b888908c46eb7fc6706607567c910
MysteriousCodingMan <l515087610@163.com>
2026-04-29 20:15:46 +0800

    智扫通Q&A问答系统

---
+++ utils/path_tool.py
@@ -0,0 +1,25 @@
+
+
+import os
+
+
+def get_project_root() -> str:
+    '''
+    获取项目根目录
+    '''
+    # 当前文件绝对路径
+    current_filr = os.path.abspath(__file__)
+    # 获取工程根目录,先获取文件所在的文件夹根目录
+    current_dit = os.path.dirname(current_filr)
+    # 获取工程根目录
+    project_root = os.path.dirname(current_dit)
+    
+    return project_root
+    
+def get_abs_path(relative_path: str) -> str:
+    '''
+    传入相对路径，获取绝对路径
+    '''
+    project_root = get_abs_path()
+    return os.path.join(project_root, relative_path)
+    
---
+++ utils/prompt_loader.py
@@ -0,0 +1,47 @@
+from utils.config_handler import prompts_conf
+from utils.path_tool import get_abs_path
+from utils.logger_handler import logger
+
+
+def load_system_prompts():
+    try:
+        system_prompt_path = get_abs_path(prompts_conf['main_prompt_path'])
+    except KeyError as e:
+        logger.error(f'[load_system_prompts]在yaml配置文件中没有main_prompt_path字段')
+        raise e
+    
+    try:
+        return open(system_prompt_path, 'r', encoding='utf-8').read()  # 测试文件是否存在且可读
+    except Exception as e:
+        logger.error(f'[load_system_prompts]加载系统提示词失败，错误信息：{str(e)}')
+        raise e
+
+
+def load_rag_prompts():
+    try:
+        rag_prompt_path = get_abs_path(prompts_conf['rag_prompt_path'])
+    except KeyError as e:
+        logger.error(f'[load_rag_prompts]在yaml配置文件中没有rag_prompt_path字段')
+        raise e
+    
+    try:
+        return open(rag_prompt_path, 'r', encoding='utf-8').read()  # 测试文件是否存在且可读
+    except Exception as e:
+        logger.error(f'[load_rag_prompts]加载RAG提示词失败，错误信息：{str(e)}')
+        raise e
+    
+    
+def load_report_prompts():
+    try:
+        report_prompt_path = get_abs_path(prompts_conf['report_prompt_path'])
+    except KeyError as e:
+        logger.error(f'[load_report_prompts]在yaml配置文件中没有report_prompt_path字段')
+        raise e
+    
+    try:
+        return open(report_prompt_path, 'r', encoding='utf-8').read()  # 测试文件是否存在且可读
+    except Exception as e:
+        logger.error(f'[load_report_prompts]加载报告提示词失败，错误信息：{str(e)}')
+        raise e
+    
+    
